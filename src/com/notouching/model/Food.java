4a89c606d76c8d2b67cc859895f5913ea29fd211
Boris Goncharov <bora4bora@gmail.com>
2020-03-26 18:40:10 -0700

    food

---
+++ src/com/notouching/model/Food.java
@@ -0,0 +1,38 @@
+package com.notouching.model;
+
+public class Food {
+    private String type;
+    private int y;
+    private int x;
+
+
+    public Food(FoodType type, int x, int y) {
+        this.type = type;
+        this.y = y;
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public FoodType getType() {
+        return type;
+    }
+
+    public void setType(FoodType type) {
+        this.type = type;
+    }
+}
---
+++ src/com/notouching/model/FoodType.java
@@ -0,0 +1,24 @@
+package com.notouching.model;
+
+public enum FoodType {
+    APPLE,
+    CARROT,
+    CHEESE,
+    ORANGE,
+    TOMATO,
+    GRAPE,
+    BROCCOLI,
+    FISH,
+    ANANAS,
+    WATEMELON,
+    SANDWICH,
+    LIMON,
+    CHICKEN,
+    FISHSTAKE,
+    AVOCADO,
+    SAUSAGE,
+    MEET,
+    CROISSANT,
+    FRUIT,
+    PIZZA
+}
